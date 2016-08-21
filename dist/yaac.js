6d93134e73a7f6272e33e00485cb50454453c24f
varun1505 <varun1505@gmail.com>
2016-08-21 23:56:50 +0530

    latest dist generated

--- dist/yaac.js
+++ dist/yaac.js
@@ -13,15 +13,18 @@
   // Modules
   angular.module('yaac.directives', []);
   angular.module('yaac.filters', []);
+  angular.module('yaac.templates', []);
   angular.module('yaac',
       [
           'yaac.config',
           'yaac.directives',
-          'yaac.filters'
+          'yaac.filters',
+          'yaac.templates'
       ]);
 
 })(angular);
 
+angular.module('yaac.templates').run(['$templateCache', function($templateCache) {$templateCache.put('/yaac/directives/yaac.template.html','<h2>Template!</h2>');}]);
 (function (angular) {
 
   angular.module('yaac.directives')
@@ -36,7 +39,7 @@
   		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
   		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
   		// template: '',
-  		templateUrl: '/yaac/src/yaac/directives/yaac.template.html', //TODO: figure out how to get a generic route
+  		templateUrl: '/yaac/directives/yaac.template.html',
   		// replace: true,
   		// transclude: true,
   		link: function($scope, iElm, iAttrs, controller) {
